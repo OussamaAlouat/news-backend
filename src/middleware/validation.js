9143b7b34ef0869d85a97b8c05f1f7ba72d0f050
OussamaAlouat <al317257@uji.es>
2019-02-02 12:42:07 +0100

    Added validation for the entities that the server recives

---
+++ src/middleware/validation.js
@@ -0,0 +1,13 @@
+import {validationResult} from "express-validator/check";
+
+const postCheckValidation = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()})
+    }
+    next()
+};
+export {
+    postCheckValidation
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import {Router} from 'express';
 import {index} from '../controller'
 import {postDocument} from "../controller/document";
+import {check} from "express-validator/check";
+import {postCheckValidation} from "../middleware/validation";
 
 export default () => {
     const routes = Router();
@@ -9,7 +11,16 @@ export default () => {
     );
 
     routes.post('/document',
-        postDocument);
+        [
+            check('title').isLength({min: 4}),
+            check('description').isLength({min: 5}),
+            check('date').exists(),
+            check('content').isLength({min: 5}),
+            check('author').isLength({min: 5}),
+            check('archiveDate').exists({checkNull: false})
+        ],
+        (req, res, next) => postCheckValidation(req, res, next),
+        (req, res) => postDocument(req, res));
 
     return routes;
 }
