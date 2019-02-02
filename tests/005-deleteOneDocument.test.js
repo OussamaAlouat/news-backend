da325851e8934d31a7a36c8184871d6e26b3fccd
OussamaAlouat <al317257@uji.es>
2019-02-02 15:57:21 +0100

    Added new test, to delete one document

--- tests/005-deleteOneDocument.test.js
+++ tests/005-deleteOneDocument.test.js
@@ -30,3 +30,49 @@ test('-------- Controller: Delete /document', (assert) => {
             server.close()
         });
 });
+
+
+test('-------- Controller: Delete /document', (assert) => {
+    const getUrl = '/documents';
+    const message = 'Status must be 200 and response must contains the id of deleted document';
+    const deleteUrl = '/document';
+
+    const statusCodeExpected = 200;
+
+    request(app)
+        .get(getUrl)
+        .expect(statusCodeExpected)
+        .then((response) => {
+            const documents = response.body.data;
+            const documentToDelete = documents[documents.length - 1];
+
+            const payload = {
+                id: documentToDelete._id
+            };
+
+            const expectedResponse = {
+                message: "Document was delete",
+                document_id: payload.id
+            };
+
+            request(app)
+                .delete(deleteUrl)
+                .send(payload)
+                .expect(statusCodeExpected)
+                .then((result) => {
+                    assert.deepEqual(result.body.response, expectedResponse, message);
+                    assert.end();
+                    server.close()
+                }, (err) => {
+                    assert.fail(err.message);
+                    assert.end();
+                    server.close()
+                });
+            server.close()
+        }, (err) => {
+            assert.fail(err.message);
+            assert.end();
+            server.close()
+        });
+
+});
