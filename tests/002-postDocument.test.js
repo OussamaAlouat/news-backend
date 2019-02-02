5b13bbeaf185cae6b27d6a93ff1c286d636cd36a
OussamaAlouat <al317257@uji.es>
2019-02-02 11:42:47 +0100

    Post a new document test

---
+++ tests/002-postDocument.test.js
@@ -0,0 +1,36 @@
+import test from 'tape';
+import request from 'supertest';
+import {app, server} from '../src/index';
+
+test('-------- Controller: Post /document', (assert) => {
+    const url = '/document';
+    const message = 'Status must be 201 and response must match with the expected simple message';
+    const responseExpected = {
+        message: "Document created correctly"
+    };
+
+    const payload = {
+        title: 'New Document' + new Date() ,
+        description: 'This is a new test document',
+        date: new Date(),
+        content: 'The content of this document is a test',
+        author: 'Oussama Alouat'
+    };
+
+    const statusCodeExpected = 201;
+    request(app)
+        .post(url)
+        .send(payload)
+        .expect(statusCodeExpected)
+        .then(
+            (res) => {
+                assert.deepEqual(res.body, responseExpected, message);
+                assert.end();
+                server.close()
+            }, (err) => {
+                assert.fail(err.message);
+                assert.end();
+                server.close()
+            }
+        );
+});
