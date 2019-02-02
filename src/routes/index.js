4021f1b1178cce5f8684d3be2e0231a5c192082e
OussamaAlouat <al317257@uji.es>
2019-02-02 11:29:03 +0100

    Added the new postDocument route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import {Router} from 'express';
 import {index} from '../controller'
+import {postDocument} from "../controller/document";
 
 export default () => {
     const routes = Router();
     routes.get('/',
-        (req, res) => index(req,res)
+        (req, res) => index(req, res)
     );
+
+    routes.post('/document',
+        postDocument);
 
     return routes;
 }
