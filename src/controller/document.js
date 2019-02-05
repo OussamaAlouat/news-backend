8e6f76e29df7c3dca41831e54b8b5ada56eb10e7
OussamaAlouat <al317257@uji.es>
2019-02-05 19:26:00 +0100

    Added the new attribute to postController

--- src/controller/document.js
+++ src/controller/document.js
@@ -1,7 +1,7 @@
 import Document from '../model/document';
 
 const postDocument = (req, res) => {
-    const {title, description, date, content, author, archiveDate} = req.body;
+    const {title, description, date, content, author, archiveDate, isArchived} = req.body;
 
     //If there are one document with the same title, description, content and author is because is the same document that
     //we will put in our database.
@@ -17,7 +17,8 @@ const postDocument = (req, res) => {
                     date: date,
                     content: content,
                     author: author,
-                    archiveDate: archiveDate
+                    archiveDate: archiveDate,
+                    isArchived: isArchived
                 });
 
                 newDocument.save((err, data) => {
